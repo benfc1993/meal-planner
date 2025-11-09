d4c70e294aee38cf0b1838251bedaa061c6af1ae
Ben Feldberg Collins <benfeldbergcollins@hotmail.co.uk>
2025-11-09 13:50:27 +0000

    fix copy command

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo cp -f ./meal-planner.service /etc/systemd/system/meal-planner.service
 make build
-sudo cp -f ./dist/ /usr/local/bin/meal-planner/
+sudo cp -fr ./dist/ /usr/local/bin/meal-planner/
 sudo systemctl enable meal-planner
 sudo systemctl restart meal-planner
