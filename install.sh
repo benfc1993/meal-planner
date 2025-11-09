7b34508cd8c9092a0acee052b8a525a9b03ac921
Ben Feldberg Collins <benfeldbergcollins@hotmail.co.uk>
2025-11-09 15:28:27 +0000

    remove db override on install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo cp -f ./meal-planner.service /etc/systemd/system/meal-planner.service
 make build
-sudo cp -fr ./dist/ /usr/local/bin/meal-planner/
+mkdir _tmp
+sudo cp -fr ./dist/meal-planner /usr/local/bin/meal-planner/meal-planner
 sudo systemctl enable meal-planner
 sudo systemctl restart meal-planner
