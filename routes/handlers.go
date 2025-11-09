64ccc0feb3ddf748f8525eb45caf89a5f0a4a1bf
Ben Feldberg Collins <benfeldbergcollins@hotmail.co.uk>
2025-11-09 16:14:47 +0000

    week navigation improvement

--- routes/handlers.go
+++ routes/handlers.go
@@ -21,16 +21,9 @@ func Handlers(f embed.FS, templates *template.Template) {
 	http.HandleFunc("/", handler(HandleHomepage))
 	http.HandleFunc("DELETE /", func(w http.ResponseWriter, r *http.Request) { w.Write(nil) })
 	http.HandleFunc("POST /recipes/generate", handler(HandleRecipesGenerate))
-	http.HandleFunc("GET /recipes/week/{week}", handler(HandleWeek))
-	http.HandleFunc("POST /week/goto", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		week := r.Form.Get("week")
 
-		r.SetPathValue("week", week)
-
-		HandleWeek(w, r, templates)
-	})
-	http.HandleFunc("POST /recipes/week", handler(HandleAddWeekRecipes))
+	http.HandleFunc("GET /week/{week}", handler(HandleWeek))
+	http.HandleFunc("POST /week", handler(HandleAddWeekRecipes))
 
 	http.HandleFunc("GET /all", func(w http.ResponseWriter, r *http.Request) {
 		templates.ExecuteTemplate(w, "/all", nil)
